e7cd09425ff3ea068834058ec53b53274a4b1e15
tobewhatwewant <tobewhatwewant@gmail.com>
2017-10-13 23:35:46 +0800

    :bug: range query support createdAt/updatedAt

--- src/createPlugin.js
+++ src/createPlugin.js
@@ -64,8 +64,8 @@ module.exports = function createPlugin(route, namespace, schema, options = {}) {
         const kw = k || s || keywords;
         const paths = Models[namespace].schema.obj;
         req.locals.query.$or = [];
-        Object.keys(paths).forEach((name) => {
-          if (paths[name].query) {
+        [Object.keys(paths), 'createdAt', 'updatedAt'].forEach((name) => {
+          if (['createdAt', 'updatedAt'].indexOf(name) !== -1 || paths[name].query) {
             // fuzzy search
             if (kw) {
               req.locals.query.$or.push({ [name]: new RegExp(kw, 'igm') });
